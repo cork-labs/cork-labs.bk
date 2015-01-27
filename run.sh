96f102a64a7eb7f2461f08295bc50fe5cb5c4755
Andre Torgal <andre.torgal@gmail.com>
2015-01-27 00:05:57 +0000

    fix(run): set pwd resolving links to run script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-node server.js
+run="$(readlink "$0")"
+cd "$(dirname "$run")"
+exec node server.js 2>&1 >> /var/log/jarvis.bk/server.js.log
 sleep 1
 echo ups >> /tmp/foo.log
